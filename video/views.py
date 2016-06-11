514d11aa3fdc50f1f350baba9d31d0a6487e9432
Leo Suarez <Leo@DTRaleigh.com>
2016-06-10 20:56:35 -0400

    Add login feature to main page. Add upload and main page with navbar links. Add logout.

--- video/views.py
+++ video/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-def home(request):
+def video_login(request):
+    #///
+    #This is the login page. The site is supposed to be password protected.
+    #\\\
+    message = 'Please log in'
+
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                message = 'Login successful.'
+
+                return HttpResponseRedirect('/main/')
+            else:
+                message = 'Account is disabled.'
+        else:
+            message = 'Invalid login.'
+
+    return render(request, 'login.html', {'message':message})
+
+def video_logout(request):
+    logout(request)
+
+    return HttpResponseRedirect('/')
+
+@login_required(login_url='/')
+def main(request):
 
     return render(request, 'index.html')
+
+@login_required(login_url='/')
+def upload(request):
+
+    return render(request, 'upload.html')
