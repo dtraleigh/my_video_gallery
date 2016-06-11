5dafa17f4728fb07fd9996cec139b049a102cbe6
Leo Suarez <Leo@DTRaleigh.com>
2016-06-10 21:13:21 -0400

    Create models, first pass.

--- video/models.py
+++ video/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class video(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name='Date added')
+    name = models.CharField(max_length=200)
+    video_date = models.DateField(verbose_name='Date taken')
+    poster = models.FileField(upload_to='uploads/poster/%Y/%m/')
+    video_file = models.FileField(upload_to='uploads/video/%Y/%m/')
+    description = models.TextField(default=None, blank=True, null=True)
+    tags = models.ManyToManyField('tag', default=None, blank=True)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+class album(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name='Date added')
+    name = models.CharField(max_length=200)
+    videos = models.ManyToManyField('video', default=None, blank=True)
+    description = models.TextField(default=None, blank=True, null=True)
+    poster = models.FileField(upload_to='uploads/poster/%Y/%m/')
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+class tag(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name='Date added')
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '%s' % (self.name)
