e7100f9409f5082372ba3a7fb702b7c3015b7a9c
Leo Suarez <Leo@DTRaleigh.com>
2016-06-11 21:12:24 -0400

    Basic layout of index.html, showing albums and their descriptions.

--- video/admin.py
+++ video/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from video.models import video, album, tag
 
 class video_admin(admin.ModelAdmin):
-    list_display = ('date_added', 'name', 'video_date', 'poster', 'video_file', 'description')
+    list_display = ('name', 'date_added', 'video_date', 'poster', 'video_file', 'description')
 
 class album_admin(admin.ModelAdmin):
-    list_display = ('date_added', 'name', 'poster', 'description')
+    list_display = ('name', 'date_added', 'poster', 'description')
 
 class tag_admin(admin.ModelAdmin):
-    list_display = ('date_added', 'name')
+    list_display = ('name', 'date_added')
 
 admin.site.register(video, video_admin)
 admin.site.register(album, album_admin)
