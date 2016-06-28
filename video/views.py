9e555c2f12a21ecca6563949057681fc65cbfc2c
Leo Suarez <Leo@DTRaleigh.com>
2016-06-27 20:32:45 -0400

    Added the view that shows videos with a certain tag.

--- video/views.py
+++ video/views.py
@@ -53,5 +53,16 @@ def video_view(request, album_id, video_id):
     this_video = video.objects.get(id=video_id)
     video_album = album.objects.get(id=album_id)
     album_videos = [v for v in video_album.videos.all()]
+    video_tags = [t for t in this_video.tags.all()]
 
-    return render(request, 'video.html', {'video':this_video, 'album':video_album, 'album_videos':album_videos})
+    return render(request, 'video.html', {'video':this_video,
+                                        'album':video_album,
+                                        'album_videos':album_videos,
+                                        'video_tags':video_tags})
+
+@login_required(login_url='/')
+def tag_view(request, tag_name):
+    videos_w_tag = video.objects.filter(tags__name=tag_name)
+    the_tag = tag.objects.get(name=tag_name)
+
+    return render(request, 'tag.html', {'videos_w_tag':videos_w_tag, 'the_tag':the_tag})
