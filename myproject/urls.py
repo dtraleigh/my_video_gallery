2727258897b3937c1d2e9c1598ed6e61696f33ff
Leo Suarez <Leo@DTRaleigh.com>
2016-07-08 19:17:03 -0400

    Tracking myproject. Last update was the change to the regex for taking in string with spaces in them.

---
+++ myproject/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.contrib import admin
+from video import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.video_login),
+    #url(r'^upload/$', views.upload),
+    url(r'^main/$', views.main),
+    url(r'^logout/$', views.video_logout),
+    url(r'^album/(?P<album_id>[0-9]+)$', views.album_view),
+    url(r'^album/(?P<album_id>[0-9]+)/video/(?P<video_id>[0-9]+)$', views.video_view),
+    url(r'^tag/(?P<tag_name>[\w|\W]+)$', views.tag_view),
+    url(r'^tag/(?P<tag_name>[\w|\W]+)/video/(?P<video_id>[0-9]+)$', views.video_tag_view),
+]
