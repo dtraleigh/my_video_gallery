f9aad964ec639a0fc2d33c1d4a01ac6289cf5b5e
Leo Suarez <Leo@DTRaleigh.com>
2017-12-04 10:55:51 -0500

    Registered the vr_shot class to the admin page.

--- video/admin.py
+++ video/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from video.models import video, album, tag
+from video.models import video, album, tag, vr_shot
 
 class video_admin(admin.ModelAdmin):
     list_display = ('name', 'date_added', 'video_date', 'poster', 'video_file')
+
+class vr_shot_admin(admin.ModelAdmin):
+    list_display = ('name', 'date_added', 'date', 'vr_file')
 
 class album_admin(admin.ModelAdmin):
     list_display = ('name', 'date_added', 'poster', 'description')
@@ -11,5 +14,6 @@ class tag_admin(admin.ModelAdmin):
     list_display = ('name', 'date_added')
 
 admin.site.register(video, video_admin)
+admin.site.register(vr_shot, vr_shot_admin)
 admin.site.register(album, album_admin)
 admin.site.register(tag, tag_admin)
