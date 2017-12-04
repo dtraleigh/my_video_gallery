096e5448e4a375f1630a457d2b31dd99c7b9e988
Leo Suarez <Leo@DTRaleigh.com>
2017-12-04 09:48:59 -0500

    Added vr_shot model

--- video/models.py
+++ video/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from video.validators import validate_poster_extension, validate_video_extension
 
+
 class video(models.Model):
     date_added = models.DateTimeField(auto_now_add=True, verbose_name='Date added')
     name = models.CharField(max_length=200)
@@ -13,12 +14,31 @@ class video(models.Model):
     lat = models.DecimalField(max_digits=9, decimal_places=6, default=0)
     lon = models.DecimalField(max_digits=9, decimal_places=6, default=0)
 
-    #Videos are ordered in descending order by the date they were taken. (newest to oldest)
+    # Videos are ordered in descending order by the date they were taken. (newest to oldest)
     class Meta:
         ordering = ['-video_date']
 
     def __str__(self):
         return '%s, taken on %s' % (self.name, self.video_date)
+
+
+class vr_video(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name='Date added')
+    name = models.CharField(max_length=200)
+    date = models.DateField(verbose_name='Date shot')
+    video_file = models.FileField(upload_to='vr/%Y/%m/')
+    description = models.TextField(default=None, blank=True, null=True)
+    tags = models.ManyToManyField('tag', default=None, blank=True)
+    lat = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    lon = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+
+    # VR shots are ordered in descending order by the date they were taken. (newest to oldest)
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return '%s, taken on %s' % (self.name, self.date)
+
 
 class album(models.Model):
     date_added = models.DateTimeField(auto_now_add=True, verbose_name='Date added')
@@ -29,6 +49,7 @@ class album(models.Model):
 
     def __str__(self):
         return '%s' % (self.name)
+
 
 class tag(models.Model):
     date_added = models.DateTimeField(auto_now_add=True, verbose_name='Date added')
