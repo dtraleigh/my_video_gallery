86b9c308d91a95b5bc6c38697d215e531e29a449
Leo Suarez <Leo@DTRaleigh.com>
2017-12-04 10:03:19 -0500

    Created the new_vr_form Form

--- video/forms.py
+++ video/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
-from video.models import video, album, tag
+from video.models import video, album, vr_shot
+
 
 class new_video_form(ModelForm):
     album = forms.ModelMultipleChoiceField(queryset=album.objects.all(), widget=forms.CheckboxSelectMultiple())
@@ -18,4 +19,23 @@ class new_video_form(ModelForm):
         widgets = {
             'video_date': forms.DateInput(attrs={'class':'datepicker'}),
             'tags':forms.CheckboxSelectMultiple(),
+        }
+
+
+class new_vr_form(ModelForm):
+    album = forms.ModelMultipleChoiceField(queryset=album.objects.all(), widget=forms.CheckboxSelectMultiple())
+
+    class Meta:
+        model = vr_shot
+        fields = ['name',
+                  'date',
+                  'vr_file',
+                  'description',
+                  'tags',
+                  'lat',
+                  'lon'
+                  ]
+        widgets = {
+            'date': forms.DateInput(attrs={'class': 'datepicker'}),
+            'tags': forms.CheckboxSelectMultiple(),
         }
