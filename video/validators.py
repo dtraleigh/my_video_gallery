bce243e37411bf9f45d6edb854491e81b5b572d7
Leo Suarez <Leo@DTRaleigh.com>
2017-12-15 16:30:30 -0500

    Added the vrview package as well as the manually downloaded build js files. Also, formatted some stuff to make PEP8 happy.

--- video/validators.py
+++ video/validators.py
@@ -2,11 +2,13 @@ import os
 
 from django.core.exceptions import ValidationError
 
+
 def validate_poster_extension(value):
     ext = os.path.splitext(value.name)[1]
-    valid_extensions = ['.jpg','.png','.gif', 'jpeg']
+    valid_extensions = ['.jpg', '.png', '.gif', 'jpeg']
     if not ext in valid_extensions:
         raise ValidationError('Not a supported image format.')
+
 
 def validate_video_extension(value):
     ext = os.path.splitext(value.name)[1]
