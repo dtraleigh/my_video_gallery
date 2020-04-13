1b5c9c9e5fe81af6392f46e2e65d26b737f708fb
Leo Suarez <leo@dtraleigh.com>
2020-04-13 14:45:45 -0400

    updated the url dispatcher moving away from url and to path.

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 from video import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.video_login),
-    url(r'^main/$', views.main),
-    url(r'^main/upload/$', views.upload),
-    url(r'^logout/$', views.video_logout),
-    url(r'^album/(?P<album_id>[0-9]+)$', views.album_view),
-    url(r'^album/(?P<album_id>[0-9]+)/(?P<shot_type>[\w|\W]+)/(?P<shot_id>[0-9]+)$', views.shot_view),
-    url(r'^tag/(?P<tag_name>[\w|\W]+)/(?P<shot_type>[\w|\W]+)/(?P<shot_id>[0-9]+)$', views.video_tag_view),
-    url(r'^tag/(?P<tag_name>[\w|\W]+)$', views.tag_view),
-    url(r'^recent/(?P<shot_type>[\w|\W]+)/(?P<shot_id>[0-9]+)$', views.recent_view),
-    url(r'^edit/(?P<shot_type>[\w|\W]+)/(?P<shot_id>[0-9]+)$', views.shot_edit_view),
-    url(r'^random/$', views.random_shot_view),
+    path("admin", admin.site.urls),
+    path(r"", views.video_login),
+    path("main/", views.main),
+    path("main/upload/", views.upload),
+    path("random/", views.random_shot_view),
+    path("logout/", views.video_logout),
+    path("album/<int:album_id>", views.album_view),
+    path("album/<int:album_id>/<str:shot_type>/<int:shot_id>", views.shot_view),
+    path("tag/<str:tag_name>/<str:shot_type>/<int:shot_id>", views.video_tag_view),
+    path("tag/<str:tag_name>", views.tag_view),
+    path("recent/<str:shot_type>/<int:shot_id>", views.recent_view),
+    path("edit/<str:shot_type>/<int:shot_id>", views.shot_edit_view),
 ]
